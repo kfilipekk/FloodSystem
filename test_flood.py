8d69e5ae86b755b00a5286c9833ee84cc49ce60c
Stanley Farrant <sf790@cam.ac.uk>
2025-02-15 19:48:23 +0000

    Task2C tests complete

--- test_flood.py
+++ test_flood.py
@@ -1,6 +1,6 @@
 """Unit tests for flood module"""
 
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
 
 # Create class of mock station with only required data for testing
 class MockStation:
@@ -31,4 +31,24 @@ def test_stations_level_over_threshold():
     assert stations_level_over_threshold(stations, 0.3) == [(station5, 10.7), (station1, 0.7), (station2, 0.5)]
     assert stations_level_over_threshold(stations, 1.5) == [(station5, 10.7)]
     assert stations_level_over_threshold(stations, 15) == []
+
+
+def test_stations_highest_rel_level():
+
+     # Build station list
+    station1 = MockStation("s1", True, 0.7)
+    station2 = MockStation("s2", True, 0.5)
+    station3 = MockStation("s3", True, -0.75)
+    station4 = MockStation("s4", False, None)
+    station5 = MockStation("s5", True, 10.7)
+
+    stations = [station1, station2, station3, station4, station5]
+
+    # Testing for different values of N
+
+    assert stations_highest_rel_level(stations, 3) == [station5, station1, station2]
+    assert stations_highest_rel_level(stations, 0) == []
+    assert stations_highest_rel_level(stations, 5) == [station5, station1, station2, station3]
+
+
     
