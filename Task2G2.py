03bda2ef5ea45f9de253f3d643b32df4ea8f7da7
Krystian Filipek <kf446@cam.ac.uk>
2025-03-02 22:42:04 +0000

    Updated floodmap

--- Task2G2.py
+++ Task2G2.py
@@ -12,6 +12,8 @@ def classify_station(station):
         return 'Below typical range'
     elif station.relative_water_level() > station.typical_range[1]:
         return 'Above typical range'
+    elif station.relative_water_level() > station.typical_range[1] * 1.5:
+        return 'Severely above typical'
     else:
         return 'Within typical range'
 
@@ -20,7 +22,7 @@ def plot_stations_on_map():
     stations = build_station_list()
     update_water_levels(stations)
     
-    ##Base mao
+    ##Base map
     map_center = [52.205281, 0.109238]
     map_ = folium.Map(location=map_center, zoom_start=6)
 
@@ -32,6 +34,8 @@ def plot_stations_on_map():
         elif status == 'Within typical range':
             color = 'blue'
         elif status == 'Above typical range':
+            color = 'orange'
+        elif status == 'Severely above typical':
             color = 'red'
         else:
             color = 'grey'
@@ -39,22 +43,55 @@ def plot_stations_on_map():
         ##Add the station marker
         folium.CircleMarker(
             location=station.coord,
-            radius=6,
+            radius=8,
             color=color,
             fill=True,
             fill_color=color,
             fill_opacity=0.7,
             popup=f"{station.name} ({station.town or 'Unknown Town'})\n{status}"
         ).add_to(map_)
-
+    
+    ##Adding a legend
+    legend_html = '''
+    <div style="position: fixed; 
+                bottom: 50px; left: 50px; width: 200px; height: 120px; 
+                background-color: white; z-index:9999; padding: 10px;
+                font-size: 14px; border: 2px solid grey; border-radius: 5px;">
+        <b>Water Level Status</b><br>
+        <i class="fa fa-circle" style="color:green"></i> Below typical<br>
+        <i class="fa fa-circle" style="color:blue"></i> Within typical<br>
+        <i class="fa fa-circle" style="color:orange"></i> Above typical<br>
+        <i class="fa fa-circle" style="color:red"></i> Severely above<br>
+        <i class="fa fa-circle" style="color:grey"></i> No data
+    </div>
+    '''
+    map_.get_root().html.add_child(folium.Element(legend_html))
+    
     ##Save the map to an HTML file, then open it in the browser
     map_.save("stations_map.html")
     print("Map saved as 'stations_map.html'")
-
+    
     return "stations_map.html"
 
 def display_map_in_browser():
     map_file = plot_stations_on_map()
     webbrowser.open(map_file)
 
-display_map_in_browser()
+def create_gui():
+    root = tk.Tk()
+    root.title("Flood Monitoring System")
+    root.geometry("300x200")
+
+    label = tk.Label(root, text="Flood Monitoring System", font=("Arial", 14))
+    label.pack(pady=10)
+
+    btn_display = tk.Button(root, text="Show Map", command=display_map_in_browser, height=2, width=15)
+    btn_display.pack(pady=10)
+
+    btn_exit = tk.Button(root, text="Exit", command=root.quit, height=2, width=15)
+    btn_exit.pack(pady=10)
+
+    root.mainloop()
+
+# Start GUI
+create_gui()
